3f9ac6e0c25cfcc8a61c1a15a738642bdae21b2d
DILIP SINGH <4ni19is029_a@nie.ac.in>
2022-12-30 23:15:42 +0530

    dom getElementByClassName is added

---
+++ DOM/getElementByClassName/app.js
@@ -0,0 +1,12 @@
+//select the element or group of elements that we want 
+//decide the effect we want to apply to the selection
+
+//getElementByClassName('classname');
+
+//HTML collections 
+
+const listItems = document.getElementsByClassName('special');
+
+console.log(listItems);
+
+listItems[2].style.color = 'blue';
